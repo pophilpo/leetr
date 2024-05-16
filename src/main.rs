c90701a6db2aa174fb91159a9882e08322ad0e5c
Popov Philipp <philolippo@gmail.com>
2024-05-16 13:54:17 +0200

    Add basic logger

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,30 @@ mod project_generator;
 mod config;
 mod project_templates;
 mod errors;
+mod logger;
 
 use clap::Parser;
+use std::process;
+
+use config::Config;
+use log::{info, error};
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let config = config::Config::new()?;
+    logger::init();
+    let config = match Config::new() {
+        Ok(project_config) => {
+            info!("Using config ~/.config/leetr/leetr.toml");
+            project_config},
+        Err(e) => {
+            error!("{}", e);
+            process::exit(1);
+        }
+    };
+
+
     let cli = argument_parser::Cli::parse();
     let title = cli.url.trim_end_matches('/').rsplit('/').nth(1).unwrap().to_string();
 

f3906f16b5efc90c6480f19f22725315b7b674af
Popov Philipp <philolippo@gmail.com>
2024-05-29 21:22:33 +0200

    Add new arguments

--- src/argument_parser.rs
+++ src/argument_parser.rs
@@ -1,8 +1,28 @@
-use clap::Parser;
+use clap::{command, Arg, ArgMatches};
 
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-pub struct Cli {
-    pub url: String,
-    pub lang: Option<String>,
+pub fn parse_args() -> ArgMatches {
+    let problem_url_arg = Arg::new("problem_url")
+        .required(true)
+        .short('u')
+        .long("url")
+        .help("Leetcode problem url");
+
+    let directory_arg = Arg::new("directory")
+        .required(false)
+        .short('d')
+        .long("dir")
+        .help("Create a directory with custom name for the problem");
+
+    let language_arg = Arg::new("language")
+        .required(false)
+        .short('l')
+        .long("lang")
+        .default_value("rust")
+        .help("Programming language used to setup the project");
+
+    command!()
+        .arg(problem_url_arg)
+        .arg(directory_arg)
+        .arg(language_arg)
+        .get_matches()
 }
